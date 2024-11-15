23d686356b1e0782986b8378e6d71c28831e3217
AlleBerta <alessandro.bertani02@gmail.com>
2024-11-15 17:04:21 +0100

    PORCODDIO

---
+++ dati_charts.php
@@ -0,0 +1,23 @@
+<!-- 
+- Temperatura, Umidità, Pressione atmosferica: Grafici a linee sono ideali, in quanto rappresentano bene le variazioni continue nel tempo.
+- Qualità dell'aria, Livello di CO2, Livello di PM2.5/PM10: Grafici a linee o a barre sovrapposte (stacked bar chart) per evidenziare la qualità e le concentrazioni.
+- Rumore, Velocità del vento, Direzione del vento: I grafici a linee o a radar (specialmente per la direzione del vento) possono visualizzare bene i dati.
+- Pioggia e Livello dell’acqua: Grafici a barre o a colonna per mostrare cumulativamente la quantità.
+- Radiazione UV, Consumo energetico: Anche qui, un grafico a linee può rappresentare bene i cambiamenti.
+- Stati come Rilevamento incendi, Illuminazione pubblica, Controllo accessi: Grafici a torta per distribuire le proporzioni, oppure un grafico a barre per visualizzare gli stati ON/OFF. 
+-->  
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chart Dati</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+
+?>
