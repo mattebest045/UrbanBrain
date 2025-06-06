96ef0b1e41053ffbd759a7a169de1b6e7ba7b286
Alessandro Bertani <alessandro.bertani02@gmail.com>
2025-06-06 12:05:10 +0200

    Inserimento import_dati per database

---
+++ server/docs/imoprt_db/creazione_evento_202506061152.sql
@@ -0,0 +1,11 @@
+INSERT INTO urbanbrain.creazione_evento (idEvento,idUtente,segnalazione,createdAt,updatedAt) VALUES
+	 (2,4,'Collaborazione con associazione locale','2025-05-21 13:41:01','2025-05-21 13:41:01'),
+	 (5,5,'Priorità media, valutare disponibilità','2025-05-13 13:41:01','2025-05-13 13:41:01'),
+	 (7,5,'Ripristino urgente richiesto','2025-05-16 13:41:01','2025-05-16 13:41:01'),
+	 (9,4,'Ripristino urgente richiesto','2025-05-10 13:41:01','2025-05-10 13:41:01'),
+	 (12,7,'Evento richiesto con urgenza','2025-05-16 13:41:01','2025-05-16 13:41:01'),
+	 (20,6,'Evento richiesto con urgenza','2025-05-30 13:41:01','2025-05-30 13:41:01'),
+	 (24,7,'Priorità media, valutare disponibilità','2025-05-29 13:41:01','2025-05-29 13:41:01'),
+	 (25,4,'Collaborazione con associazione locale','2025-05-05 13:41:01','2025-05-05 13:41:01'),
+	 (27,7,'Richiesta dal Comune','2025-05-10 13:41:01','2025-05-10 13:41:01'),
+	 (30,6,'Evento educativo necessario','2025-05-04 13:41:01','2025-05-04 13:41:01');
